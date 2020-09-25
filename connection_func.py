df2f87624d1c911001d08d90122eab087a3978e1
petrovad <petrovad@electronika.ru>
2020-09-25 13:46:11 +0300

    from office

--- connection_func.py
+++ connection_func.py
@@ -7,9 +7,9 @@ class ConnectToJSON:
 
     def connect(self, data_file):
         with open(data_file) as json_file:
-            self.__data = json.load(json_file)
+            return json.load(json_file)
 
-    def get_data(self, name):
-        for student in self.__data["students"]:
-            if student["name"] == name:
-                return student
+    # def get_data(self, name):
+    #     for student in self.__data["students"]:
+    #         if student["name"] == name:
+    #             return student
--- test_entities.py
+++ test_entities.py
@@ -7,55 +7,50 @@ api = "/v2/entities"
 url = "http://172.26.66.74:1026"
 
 
-with open('data.json') as f:
-    templates = json.load(f)
-print(templates)
-
-# 12
-# def test_create_entity_and_get_code_204():
-#     json_file = ConnectToJSON()
-#     response = requests.post(f"{url}{api}", json=json_file.connect('data.json'))
-#     assert response.status_code == 201 or 204
-
-
-def test_get_list_entities_and_code_200():
-    response = requests.get(f"{url}{api}")
-    assert response.status_code == 200
-
-
-def test_assert():
+def test_create_entity():
     json_file = ConnectToJSON()
-    # dict1 = json_file.connect('data.json')
-    response = requests.get(f"{url}{api}")
+    templates = json_file.connect('data.json')
+
+    response = requests.post(f"{url}{api}", json=templates)
+    assert response.status_code in (201, 204)
+
+    response = requests.get(f"{url}{api}?id={templates['id']}")
+    assert response.status_code == 200
     response_body = response.json()
-    assert response_body["type"] == "Room"
+    assert response_body[0]["type"] == templates["type"]
+    assert response_body[0]["id"] == templates["id"]
+    assert response_body[0]["temperature"]["value"] == templates["temperature"]["value"] and (
+        (type(response_body[0]["temperature"]["value"]) in (float, int)))
+    assert response_body[0]["humidity"]["value"] == templates["humidity"]["value"] and (
+            (type(templates["humidity"]["value"])) in (float, int))
+    assert response_body[0]["location"]["value"] == templates["location"]["value"]
+    assert response_body[0]["location"]["type"] == templates["location"]["type"]
+    assert response_body[0]["location"]["metadata"]["crs"]["value"] == templates["location"]["metadata"]["crs"]["value"]
 
-
-
-# def test_get_check_content_type_equals_json():
-#     response = requests.get(url + api)
-#     assert response.headers['Content-Type'] == "application/json"
-#
-#
-# def test_get_entities_url_equals_entities():
-#     response = requests.get(url + api)
-#     response_body = response.json()
-#     assert response_body["entities_url"] == "/v2/entities"
-#
-#
-# def test_get_types_url_equals_types():
-#     response = requests.get(url + api)
-#     response_body = response.json()
-#     assert response_body["types_url"] == "/v2/types"
-#
-#
-# def test_get_subscriptions_url_equals_subscriptions():
-#     response = requests.get(url + api)
-#     response_body = response.json()
-#     assert response_body["subscriptions_url"] == "/v2/subscriptions"
-#
-#
-# def test_get_registrations_url_equals_registrations():
-#     response = requests.get(url + api)
-#     response_body = response.json()
-#     assert response_body["registrations_url"] == "/v2/registrations"
+    # def test_get_check_content_type_equals_json():
+    #     response = requests.get(url + api)
+    #     assert response.headers['Content-Type'] == "application/json"
+    #
+    #
+    # def test_get_entities_url_equals_entities():
+    #     response = requests.get(url + api)
+    #     response_body = response.json()
+    #     assert response_body["entities_url"] == "/v2/entities"
+    #
+    #
+    # def test_get_types_url_equals_types():
+    #     response = requests.get(url + api)
+    #     response_body = response.json()
+    #     assert response_body["types_url"] == "/v2/types"
+    #
+    #
+    # def test_get_subscriptions_url_equals_subscriptions():
+    #     response = requests.get(url + api)
+    #     response_body = response.json()
+    #     assert response_body["subscriptions_url"] == "/v2/subscriptions"
+    #
+    #
+    # def test_get_registrations_url_equals_registrations():
+    #     response = requests.get(url + api)
+    #     response_body = response.json()
+    #     assert response_body["registrations_url"] == "/v2/registrations"
