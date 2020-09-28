790ae038cd1455aa4019e9fb0bb7ed2625737a09
petrovad <petrovad@electronika.ru>
2020-09-28 17:24:42 +0300

    from office

--- test_entities.py
+++ test_entities.py
@@ -10,20 +10,22 @@ url = "http://172.26.66.74:1026"
 # templates = json_file.open_json('entity_room.json')
 
 
-@pytest.fixture
-def setup_module(json):
-    json_file = ConnectToJSON()
-    templates = json_file.open_json(json)
-    return templates
+# @pytest.fixture(scope="module")
+# def setup_module():
+#     json_file = ConnectToJSON()
+#     templates = json_file.open_json('entity_room.json')
+#     return templates
+#
+#
+# print(setup_module())
 
-print(setup_module('entity_room.json'))
 
-
-def test_create_entity(templates):
+def test_create_entity():
     # json_file = ConnectToJSON()
     # templates = json_file.open_json('entity_room.json')
     # POST запрос /v2/entities
-
+    json_file = ConnectToJSON()
+    templates = json_file.open_json('entity_room.json')
     response = requests.post(f"{url}{api}", json=templates)
     assert response.status_code in (201, 204)
     # Проверка корректности создания /v2/entities/{entityId}
@@ -55,46 +57,57 @@ def test_create_entity(templates):
     response = requests.get(f"{url}{api}/{templates['id']}/attrs")
     assert response.status_code == 404
 
-# def test_replace_all_entity_attributes():
-#
-#     response = requests.post(f"{url}{api}", json=templates)
-#     assert response.status_code in (201, 204)
-#     json_file_for_replace = ConnectToJSON()
-#     templates_for_replace = json_file.open_json('entity_room_replace.json')
-#     response = requests.put(f"{url}{api}/{templates['id']}/attrs", json=templates_for_replace)
-#     assert response.status_code == 204
-#     # Проверка замены атрибутов
-#     response = requests.get(f"{url}{api}/{templates['id']}/attrs")
-#     response_body = response.json()
-#     assert response_body["temperature"]["value"] == templates_for_replace["temperature"]["value"] and (
-#         (type(response_body["temperature"]["value"]) in (float, int)))
-#     assert response_body["seatNumber"]["value"] == templates_for_replace["seatNumber"]["value"] and (
-#         (type(response_body["seatNumber"]["value"]) in (float, int)))
-#     response = requests.delete(f"{url}{api}/{templates['id']}")
-#     assert response.status_code == 204
-#     response = requests.get(f"{url}{api}/{templates['id']}/attrs")
-#     assert response.status_code == 404
-#
-#
-# def test_update_or_append_entity_attributes():
-#     templates = setup_module('entity_room.json')
-#
-#     response = requests.post(f"{url}{api}", json=templates)
-#     assert response.status_code in (201, 204)
-#
-#     templates_for_append = setup_module('entity_room_update_or_append.json')
-#     # POST запрос /v2/entities/{entityId}/attrs
-#     # Update or Append Entity Attributes
-#     response = requests.post(f"{url}{api}/{templates['id']}/attrs", json=templates_for_append)
-#     assert response.status_code == 204
-#     response = requests.get(f"{url}{api}/{templates['id']}/attrs")
-#     response_body = response.json()
-#     assert response_body["ambientNoise"]["value"] == templates_for_append["ambientNoise"]["value"] and (
-#         (type(response_body["ambientNoise"]["value"]) in (float, int)))
-#     response = requests.delete(f"{url}{api}/{templates['id']}")
-#     assert response.status_code == 204
-#     response = requests.get(f"{url}{api}/{templates['id']}/attrs")
-#     assert response.status_code == 404
+
+
+def test_replace_all_entity_attributes():
+    json_file = ConnectToJSON()
+    templates = json_file.open_json('entity_room.json')
+
+    response = requests.post(f"{url}{api}", json=templates)
+    assert response.status_code in (201, 204)
+    json_file_for_replace = ConnectToJSON()
+    templates_for_replace = json_file.open_json('entity_room_replace.json')
+    response = requests.put(f"{url}{api}/{templates['id']}/attrs", json=templates_for_replace)
+    assert response.status_code == 204
+    # Проверка замены атрибутов
+    response = requests.get(f"{url}{api}/{templates['id']}/attrs")
+    response_body = response.json()
+    assert response_body["temperature"]["value"] == templates_for_replace["temperature"]["value"] and (
+        (type(response_body["temperature"]["value"]) in (float, int)))
+    assert response_body["seatNumber"]["value"] == templates_for_replace["seatNumber"]["value"] and (
+        (type(response_body["seatNumber"]["value"]) in (float, int)))
+    response = requests.delete(f"{url}{api}/{templates['id']}")
+    assert response.status_code == 204
+    response = requests.get(f"{url}{api}/{templates['id']}/attrs")
+    assert response.status_code == 404
+
+
+def test_update_or_append_entity_attributes():
+    # templates = setup_module('entity_room.json')
+    json_file = ConnectToJSON()
+    templates = json_file.open_json('entity_room.json')
+
+    response = requests.post(f"{url}{api}", json=templates)
+    assert response.status_code in (201, 204)
+
+    json_file_for_append = ConnectToJSON()
+    templates_for_append = json_file_for_append.open_json('entity_room_update_or_append.json')
+
+    # templates_for_append = setup_module('entity_room_update_or_append.json')
+    # POST запрос /v2/entities/{entityId}/attrs
+    # Update or Append Entity Attributes
+    response = requests.post(f"{url}{api}/{templates['id']}/attrs", json=templates_for_append)
+    assert response.status_code == 204
+    response = requests.get(f"{url}{api}/{templates['id']}/attrs")
+    response_body = response.json()
+    assert response_body["ambientNoise"]["value"] == templates_for_append["ambientNoise"]["value"] and (
+        (type(response_body["ambientNoise"]["value"]) in (float, int)))
+    response = requests.delete(f"{url}{api}/{templates['id']}")
+    assert response.status_code == 204
+    response = requests.get(f"{url}{api}/{templates['id']}/attrs")
+    assert response.status_code == 404
+
+
 
 # def test_delete_entity():
 #     json_file = ConnectToJSON()
